e8209d8678cc754bbb258b8a629165a83e40e254
Faraz <farazgojali@gmail.com>
2022-11-03 13:45:43 +0500

    api response issue fixes

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,7 @@ app.post("/add-product", async (req, resp) => {
 })
 
 app.get("/products", async (req, resp) => {
-    let products = Product.find();
+    let products = await Product.find();
     if (products.length > 0) {
         resp.send(products);
     } else {
