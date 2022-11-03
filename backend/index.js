03b76d970be2e28db25a077f2ba75767a1300e8b
Faraz <farazgojali@gmail.com>
2022-11-03 13:36:48 +0500

    test

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require('./db/config');
 const User = require("./db/User");
 const Product = require("./db/Product");
+const { restart } = require('nodemon');
 const app = express();
 
 app.use(express.json());
@@ -35,6 +36,15 @@ app.post("/add-product", async (req, resp) => {
     let product = new Product(req.body);
     let result = await product.save();
     resp.send(result)
+})
+
+app.get("/products", async (req, resp) => {
+    let products = Product.find();
+    if (products.length > 0) {
+        resp.send(products);
+    } else {
+        resp.send({ result: "No Products found." })
+    }
 })
 
 app.listen(5000);
